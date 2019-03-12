8d7f53beb501fcec4e0cf7a301dc17581c41f247
Re <47809253+redrgnl@users.noreply.github.com>
2019-03-13 00:12:00 +0700

    Aboutme

--- BackEnd/login/connection.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$connect = mysqli_connect("localhost","root","","doctor_asistant");
- 
-// Check connection
-if (mysqli_connect_errno()){
-	echo "Koneksi database gagal : " . mysqli_connect_error();
-}
- 
-?>
--- BackEnd/pages/php/404.php
+++
@@ -1,5 +0,0 @@
-<section class="content-header">
-<h1>404 Not Found</h1>
-
-<p style="font-size: 30px">Web Page Not Available</p>
-</section>
